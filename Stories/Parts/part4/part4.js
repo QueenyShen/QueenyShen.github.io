0ac30f72f7eafe862b6f2dc362a537d59de22cc5
Queeny <shenk574@newschool.edu>
2023-02-11 17:20:12 -0500

    worked on part 6

---
+++ Stories/Parts/part4/part4.js
@@ -0,0 +1,55 @@
+var controller = new ScrollMagic.Controller();
+
+new ScrollMagic.Scene({
+    triggerElement: "#trigger1",
+    triggerHook: 0.3, // show, when scrolled 10% into view
+    duration: "50%", // hide 10% before exiting view (80% + 10% from bottom)
+    offset: 50 // move trigger to center of element
+})
+.setClassToggle("#reveal1", "visible") // add class to reveal
+// .addIndicators() // add indicators (requires plugin)
+.addTo(controller);
+
+
+new ScrollMagic.Scene({
+    triggerElement: "#trigger2",
+    triggerHook: 0, // show, when scrolled 10% into view
+    duration: "70%", // hide 10% before exiting view (80% + 10% from bottom)
+    offset: 50 // move trigger to center of element
+})
+.setClassToggle("#reveal2", "visible") // add class to reveal
+// .addIndicators() // add indicators (requires plugin)
+.addTo(controller);
+
+
+new ScrollMagic.Scene({
+    triggerElement: "#trigger3",
+    triggerHook: 0.1, // show, when scrolled 10% into view
+    duration: "70%", // hide 10% before exiting view (80% + 10% from bottom)
+    offset: 50 // move trigger to center of element
+})
+.setClassToggle("#reveal3", "visible") // add class to reveal
+// .addIndicators() // add indicators (requires plugin)
+.addTo(controller);
+
+
+new ScrollMagic.Scene({
+    triggerElement: "#trigger4",
+    triggerHook: 0, // show, when scrolled 10% into view
+    duration: "50%", // hide 10% before exiting view (80% + 10% from bottom)
+    offset: 50 // move trigger to center of element
+})
+.setClassToggle("#reveal4", "visible") // add class to reveal
+// .addIndicators() // add indicators (requires plugin)
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#trigger5",
+    triggerHook: 1, // show, when scrolled 10% into view
+    duration: "100%", // hide 10% before exiting view (80% + 10% from bottom)
+    offset: 50 // move trigger to center of element
+})
+.setClassToggle("#reveal4", "visible") // add class to reveal
+// .addIndicators() // add indicators (requires plugin)
+.addTo(controller);
+
