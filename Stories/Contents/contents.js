ced02b799807e56b968793faeb4cad554e1a3c5a
Queeny <shenk574@newschool.edu>
2023-02-13 21:18:51 -0500

    fixed doors

--- Stories/Contents/contents.js
+++ Stories/Contents/contents.js
@@ -1,6 +1,59 @@
-var element = document.querySelector(".door");
-element.addEventListener("click", toggleDoor);
+var element1 = document.querySelector(".door");
+element1.addEventListener("click", toggleDoor1);
 
-function toggleDoor() {
-  element.classList.toggle("doorOpen");
+function toggleDoor1() {
+  element1.classList.toggle("doorOpen");
+}
+
+var element2 = document.querySelector(".door2");
+element2.addEventListener("click", toggleDoor2);
+
+
+function toggleDoor2() {
+  element2.classList.toggle("doorOpen");
+}
+
+var element3 = document.querySelector(".door3");
+element3.addEventListener("click", toggleDoor3);
+
+function toggleDoor3() {
+  element3.classList.toggle("doorOpen");
+}
+
+var element4 = document.querySelector(".door4");
+element4.addEventListener("click", toggleDoor4);
+
+
+function toggleDoor4() {
+  element4.classList.toggle("doorOpen");
+}
+
+var element5 = document.querySelector(".door5");
+element5.addEventListener("click", toggleDoor5);
+
+function toggleDoor5() {
+  element5.classList.toggle("doorOpen");
+}
+
+var element6 = document.querySelector(".door6");
+element6.addEventListener("click", toggleDoor6);
+
+
+function toggleDoor6() {
+  element6.classList.toggle("doorOpen");
+}
+
+var element7 = document.querySelector(".door7");
+element7.addEventListener("click", toggleDoor7);
+
+function toggleDoor7() {
+  element7.classList.toggle("doorOpen");
+}
+
+var element8 = document.querySelector(".door8");
+element8.addEventListener("click", toggleDoor8);
+
+
+function toggleDoor8() {
+  element8.classList.toggle("doorOpen");
 }
