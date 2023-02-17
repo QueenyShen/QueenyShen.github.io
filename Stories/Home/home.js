79a5492ac64d0f3368f19f691788ad9eea967e52
Queeny <shenk574@newschool.edu>
2023-02-17 10:22:56 -0500

    create peephole

--- Stories/Home/home.js
+++ Stories/Home/home.js
@@ -4,3 +4,12 @@ element.addEventListener("click", toggleDoor);
 function toggleDoor() {
   element.classList.toggle("doorOpen");
 }
+
+var popupWindow = null;
+function positionedPopup(url,winName,w,h,t,l,scroll){
+settings =
+'height='+h+',width='+w+',top='+t+',left='+l+',scrollbars='+scroll+',resizable'
+popupWindow = window.open(url,winName,settings)
+}
+$(document).ready(function() {$(".entry").draggable(); })
+Splitting();
