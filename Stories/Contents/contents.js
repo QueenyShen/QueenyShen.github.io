051a9f4bae2ff0d01e4aa6cdfe11d4bfb9d8cea7
Queeny <shenk574@newschool.edu>
2023-02-18 13:05:56 -0500

    contents page delay link

--- Stories/Contents/contents.js
+++ Stories/Contents/contents.js
@@ -54,3 +54,100 @@ function toggleDoor8() {
   element8.classList.toggle("doorOpen");
 }
 
+
+const door = document.querySelector('.door');
+
+door.addEventListener('click', (event) => {
+  event.preventDefault(); // Prevent default link behavior
+
+  const doorLink = event.currentTarget.closest('.door-link');
+
+  setTimeout(() => {
+    window.location.href = doorLink.href; // Navigate to the link after delay
+  }, 1000); // Delay for 1 second (1000 milliseconds)
+});
+
+
+const door2 = document.querySelector('.door2');
+
+door2.addEventListener('click', (event) => {
+  event.preventDefault(); // Prevent default link behavior
+
+  const doorLink2 = event.currentTarget.closest('.door-link2');
+
+  setTimeout(() => {
+    window.location.href = doorLink2.href; // Navigate to the link after delay
+  }, 1000); // Delay for 1 second (1000 milliseconds)
+});
+
+const door3 = document.querySelector('.door3');
+
+door3.addEventListener('click', (event) => {
+  event.preventDefault(); // Prevent default link behavior
+
+  const doorLink3 = event.currentTarget.closest('.door-link3');
+
+  setTimeout(() => {
+    window.location.href = doorLink3.href; // Navigate to the link after delay
+  }, 1000); // Delay for 1 second (1000 milliseconds)
+});
+
+const door4 = document.querySelector('.door4');
+
+door4.addEventListener('click', (event) => {
+  event.preventDefault(); // Prevent default link behavior
+
+  const doorLink4 = event.currentTarget.closest('.door-link4');
+
+  setTimeout(() => {
+    window.location.href = doorLink4.href; // Navigate to the link after delay
+  }, 1000); // Delay for 1 second (1000 milliseconds)
+});
+
+const door5 = document.querySelector('.door5');
+
+door5.addEventListener('click', (event) => {
+  event.preventDefault(); // Prevent default link behavior
+
+  const doorLink5 = event.currentTarget.closest('.door-link5');
+
+  setTimeout(() => {
+    window.location.href = doorLink5.href; // Navigate to the link after delay
+  }, 1000); // Delay for 1 second (1000 milliseconds)
+});
+
+const door6 = document.querySelector('.door6');
+
+door2.addEventListener('click', (event) => {
+  event.preventDefault(); // Prevent default link behavior
+
+  const doorLink6 = event.currentTarget.closest('.door-link6');
+
+  setTimeout(() => {
+    window.location.href = doorLink6.href; // Navigate to the link after delay
+  }, 1000); // Delay for 1 second (1000 milliseconds)
+});
+
+const door7 = document.querySelector('.door7');
+
+door2.addEventListener('click', (event) => {
+  event.preventDefault(); // Prevent default link behavior
+
+  const doorLink7 = event.currentTarget.closest('.door-link7');
+
+  setTimeout(() => {
+    window.location.href = doorLink7.href; // Navigate to the link after delay
+  }, 1000); // Delay for 1 second (1000 milliseconds)
+});
+
+const door8 = document.querySelector('.door8');
+
+door8.addEventListener('click', (event) => {
+  event.preventDefault(); // Prevent default link behavior
+
+  const doorLink8 = event.currentTarget.closest('.door-link8');
+
+  setTimeout(() => {
+    window.location.href = doorLink8.href; // Navigate to the link after delay
+  }, 1000); // Delay for 1 second (1000 milliseconds)
+});
