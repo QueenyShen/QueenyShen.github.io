f437e8947d4da911ab84383e22878554baa2dcb5
Queeny <shenk574@newschool.edu>
2023-02-19 19:34:58 -0500

    change backgrounds

--- Stories/Parts/part2/part2.js
+++ Stories/Parts/part2/part2.js
@@ -1,13 +1,28 @@
+// window.onload = function() {
+//     window.scrollTo(0,document.body.scrollHeight);
+//   }
+
+//   function reverseWords(str) {
+//     return str.split(" ").reverse().join(" ");
+//   }
+//   document.addEventListener("DOMContentLoaded", function() {
+//     var text = document.getElementById("text");
+//     text.innerHTML = reverseWords(text.innerHTML);
+//   });
+
+
+
 window.onload = function() {
-    window.scrollTo(0,document.body.scrollHeight);
-  }
+  window.scrollTo(0,document.body.scrollHeight);
+}
 
-  function reverseWords(str) {
-    return str.split(" ").reverse().join(" ");
-  }
-  document.addEventListener("DOMContentLoaded", function() {
-    var text = document.getElementById("text");
-    text.innerHTML = reverseWords(text.innerHTML);
-  });
+// function reverseWords(str) {
+//   return str.split(" ").reverse().join(" ");
+// }
 
-  
+// document.addEventListener("DOMContentLoaded", function() {
+//   var textElements = document.getElementsByClassName("text");
+//   for (var i = 0; i < textElements.length; i++) {
+//     textElements[i].innerHTML = reverseWords(textElements[i].innerHTML);
+//   }
+// });
