033cc9a63ed612bee3f49e96ce6ec92eaa6f573c
Queeny <shenk574@newschool.edu>
2023-03-23 16:04:49 -0400

    clean up code

--- projects/ElasticCollection/mainpage.js
+++ projects/ElasticCollection/mainpage.js
@@ -1,3 +1,118 @@
+// working code
+const show = (collection, filter = '') => {
+  const filteredItems = filter ? collection.filter(item => item.place === filter) : collection;
+
+  // Get the `ul` where the items will be inserted
+  const collectionList = document.getElementById('collection')
+  collectionList.innerHTML = '' // Clear all items before showing filtered items
+
+  filteredItems.forEach(item => {
+    const listItem = document.createElement('li') // Make the `li`
+    const itemImage = document.createElement('img') // And an image
+    itemImage.src = item.bookCover // Set the `src` attribute from the JSON
+
+    itemImage.style.position = 'absolute' 
+
+    const container = document.getElementById('collection')
+    const x = Math.random() * container.offsetWidth
+    const y = Math.random() * container.offsetHeight
+    itemImage.style.left = `${x}px`
+    itemImage.style.top = `${y}px`
+
+    listItem.appendChild(itemImage) // And add that too
+    collectionList.appendChild(listItem) // Then add the whole `li` into the `ul`
+
+    // Set up the image to move at a set speed
+    let dx = Math.random() * 2 - 1 
+    let dy = Math.random() * 2 - 1 
+    let xPos = x
+    let yPos = y
+    setInterval(() => {
+      xPos += dx * 0.8 // Update x position
+      yPos += dy * 0.8 // Update y position
+      
+      // Check if the image is still within the container
+      if (xPos < 0 || xPos > container.offsetWidth) {
+        dx = -dx // Invert x velocity
+      }
+      if (yPos < 0 || yPos > container.offsetHeight) {
+        dy = -dy // Invert y velocity
+      }
+      // Set new position of image
+      itemImage.style.left = `${xPos}px`
+      itemImage.style.top = `${yPos}px`
+    }, 30) // Update position every 30 milliseconds
+
+    
+  })
+}
+
+const remove = () => {
+  const collectionList = document.getElementById('collection')
+  collectionList.innerHTML = '' // Clear all items from the container
+}
+
+const allButton = document.querySelector('.left-items .button:nth-child(1)')
+allButton.addEventListener('click', () => {
+  fetch('collection.json')
+    .then(response => response.json())
+    .then(collection => {
+      show(collection) 
+    })
+})
+
+
+const mineButton = document.querySelector('.left-items .button:nth-child(2)')
+mineButton.addEventListener('click', () => {
+  fetch('collection.json')
+    .then(response => response.json())
+    .then(collection => {
+      remove() // Clear all items from the container
+      show(collection, 'My books') // Show only items with "place" equal to "My books"
+    })
+})
+
+
+const friendButton = document.querySelector('.left-items .button:nth-child(3)')
+friendButton.addEventListener('click', () => {
+  fetch('collection.json')
+    .then(response => response.json())
+    .then(collection => {
+      remove() // Clear all items from the container
+      show(collection, 'Friends books')
+    })
+})
+
+const libraryButton = document.querySelector('.left-items .button:nth-child(4)')
+libraryButton.addEventListener('click', () => {
+  fetch('collection.json')
+    .then(response => response.json())
+    .then(collection => {
+      remove() // Clear all items from the container
+      show(collection, 'School library')
+    })
+})
+
+const storeButton = document.querySelector('.left-items .button:nth-child(5)')
+storeButton.addEventListener('click', () => {
+  fetch('collection.json')
+    .then(response => response.json())
+    .then(collection => {
+      remove() // Clear all items from the container
+      show(collection, 'Book store')
+    })
+})
+
+
+// Show all items when page is first visited
+fetch('collection.json')
+  .then(response => response.json())
+  .then(collection => {
+    show(collection)
+  })
+
+
+
 // //test code
 // const renderItems = (collection, speed) => {
 //   // The `ul` where the items will be inserted
@@ -87,173 +202,3 @@
 
 
 
-//  //original code
-// const renderItems = (collection, speed) => {
-//   // The `ul` where the items will be inserted
-//   const collectionList = document.getElementById('collection')
-
-//   // Loop through each item in the collection array
-//   collection.forEach(item => {
-//     const listItem = document.createElement('li') // Make the `li`
-//     const itemImage = document.createElement('img') // And an image
-//     itemImage.src = item.bookCover // Set the `src` attribute from the JSON
-
-    
-//     itemImage.style.position = 'absolute' // Set the position of the image to absolute
-
-//     const container = document.getElementById('collection')
-//     const x = Math.random() * container.offsetWidth
-//     const y = Math.random() * container.offsetHeight
-//     itemImage.style.left = `${x}px`
-//     itemImage.style.top = `${y}px`
-
-//     listItem.appendChild(itemImage) // And add that too
-//     collectionList.appendChild(listItem) // Then add the whole `li` into the `ul`
-
-//     // Set up the image to move at a set speed
-//     let dx = Math.random() * 4 - 1 
-//     let dy = Math.random() * 4 - 1 
-//     let xPos = x
-//     let yPos = y
-//     setInterval(() => {
-//       xPos += dx * speed // Update x position
-//       yPos += dy * speed // Update y position
-      
-//       // Check if the image is still within the container
-//       if (xPos < 0 || xPos > container.offsetWidth) {
-//         dx = -dx // Invert x velocity
-//       }
-//       if (yPos < 0 || yPos > container.offsetHeight) {
-//         dy = -dy // Invert y velocity
-//       }
-//       // Set new position of image
-//       itemImage.style.left = `${xPos}px`
-//       itemImage.style.top = `${yPos}px`
-//     }, 50) // Update position every 50 milliseconds
-    
-//   })
-// }
-
-
-
-// fetch('collection.json')
-//   .then(response => response.json())
-//   .then(collection => {
-//     // And passes the data to the function, above!
-//     renderItems(collection, 0.8) 
-//   })
-
-
-const show = (collection, filter = '') => {
-  const filteredItems = filter ? collection.filter(item => item.place === filter) : collection;
-
-  // Get the `ul` where the items will be inserted
-  const collectionList = document.getElementById('collection')
-  collectionList.innerHTML = '' // Clear all items before showing filtered items
-
-  filteredItems.forEach(item => {
-    const listItem = document.createElement('li') // Make the `li`
-    const itemImage = document.createElement('img') // And an image
-    itemImage.src = item.bookCover // Set the `src` attribute from the JSON
-
-    itemImage.style.position = 'absolute' // Set the position of the image to absolute
-
-    const container = document.getElementById('collection')
-    const x = Math.random() * container.offsetWidth
-    const y = Math.random() * container.offsetHeight
-    itemImage.style.left = `${x}px`
-    itemImage.style.top = `${y}px`
-
-    listItem.appendChild(itemImage) // And add that too
-    collectionList.appendChild(listItem) // Then add the whole `li` into the `ul`
-
-    // Set up the image to move at a set speed
-    let dx = Math.random() * 2 - 1 
-    let dy = Math.random() * 2 - 1 
-    let xPos = x
-    let yPos = y
-    setInterval(() => {
-      xPos += dx * 0.8 // Update x position
-      yPos += dy * 0.8 // Update y position
-      
-      // Check if the image is still within the container
-      if (xPos < 0 || xPos > container.offsetWidth) {
-        dx = -dx // Invert x velocity
-      }
-      if (yPos < 0 || yPos > container.offsetHeight) {
-        dy = -dy // Invert y velocity
-      }
-      // Set new position of image
-      itemImage.style.left = `${xPos}px`
-      itemImage.style.top = `${yPos}px`
-    }, 30) // Update position every 10 milliseconds
-
-    
-  })
-}
-
-const remove = () => {
-  const collectionList = document.getElementById('collection')
-  collectionList.innerHTML = '' // Clear all items from the container
-}
-
-const allButton = document.querySelector('.left-items .button:nth-child(1)')
-allButton.addEventListener('click', () => {
-  fetch('collection.json')
-    .then(response => response.json())
-    .then(collection => {
-      show(collection) 
-    })
-})
-
-
-const mineButton = document.querySelector('.left-items .button:nth-child(2)')
-mineButton.addEventListener('click', () => {
-  fetch('collection.json')
-    .then(response => response.json())
-    .then(collection => {
-      remove() // Clear all items from the container
-      show(collection, 'My books') // Show only items with "place" equal to "My books"
-    })
-})
-
-
-const friendButton = document.querySelector('.left-items .button:nth-child(3)')
-friendButton.addEventListener('click', () => {
-  fetch('collection.json')
-    .then(response => response.json())
-    .then(collection => {
-      remove() // Clear all items from the container
-      show(collection, 'Friends books')
-    })
-})
-
-const libraryButton = document.querySelector('.left-items .button:nth-child(4)')
-libraryButton.addEventListener('click', () => {
-  fetch('collection.json')
-    .then(response => response.json())
-    .then(collection => {
-      remove() // Clear all items from the container
-      show(collection, 'School library')
-    })
-})
-
-const storeButton = document.querySelector('.left-items .button:nth-child(5)')
-storeButton.addEventListener('click', () => {
-  fetch('collection.json')
-    .then(response => response.json())
-    .then(collection => {
-      remove() // Clear all items from the container
-      show(collection, 'Book store')
-    })
-})
-
-
-// Show all items when page is first visited
-fetch('collection.json')
-  .then(response => response.json())
-  .then(collection => {
-    show(collection)
-  })
-
-  
--- projects/ElasticCollection/mainpage2.js
+++ projects/ElasticCollection/mainpage2.js
@@ -1,55 +1,53 @@
+const renderItems = (collection) => {
+    // The `ul` where the items will be inserted
+    const collectionList = document.getElementById('collection')
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const images = [];
+    // Shuffle the collection array
+    const shuffledCollection = shuffleArray(collection);
 
-
-// Load the image data from a JSON file
-fetch('collection.json')
-  .then(response => response.json())
-  .then(data => {
-    // Loop through each item in the collection and create an Image object for it
-    data.forEach(item => {
-      const img = new Image();
-      img.src = item.bookCover;
-      // When the image is loaded, add it to the images array
-
-      img.onload = () => {
-        images.push({
-          x: Math.random() * canvas.width,
-          y: Math.random() * canvas.height,
-          dx: Math.random() * 2 - 1,
-          dy: Math.random() * 2 - 1,
-          image: img
-        });
-      };
-    });
-  });
-
-function draw() {
-  // Clear the canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Loop through each image and draw it on the canvas
-  images.forEach(image => {
-    ctx.drawImage(image.image, image.x, image.y, 10, 10);
-
-    // Update the image's position
-    image.x += image.dx;
-    image.y += image.dy;
-
-    // Bounce the image off the edges of the canvas
-    if (image.x < 0 || image.x + 10 > canvas.width) {
-      image.dx = -image.dx;
-    }
-    if (image.y < 0 || image.y + 10 > canvas.height) {
-      image.dy = -image.dy;
-    }
-  });
-
-  // Request the next frame
-  requestAnimationFrame(draw);
+    // Loop through each item in the shuffled collection array
+    shuffledCollection.forEach(item => {
+        const listItem = document.createElement('li') // Make the `li`
+        const itemImage = document.createElement('img') // And an image
+        itemImage.src = item.bookCover// Set the `src` attribute from the JSON
+        listItem.appendChild(itemImage) // And add that too
+        collectionList.appendChild(listItem) // Then add the whole `li` into the `ul'
+    })  
 }
 
-// Start the animation loop
-requestAnimationFrame(draw);
+// Shuffle array function
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+// Fetch gets your JSON file…
+fetch('collection.json')
+	.then(response => response.json())
+	.then(collection => {
+		// And passes the data to the function, above!
+		renderItems(collection) // In reverse order
+	})
+
+	const zoomArea = document.getElementById("zoom-area");
+	const collectionList = document.getElementById('collection');
+	
+	zoomArea.addEventListener("click", (event) => {
+	  zoomArea.classList.toggle("zoom");
+	  if (zoomArea.classList.contains("zoom")) {
+		const x = event.clientX; // Get the x-coordinate of the click
+		const y = event.clientY; // Get the y-coordinate of the click
+		const rect = collectionList.getBoundingClientRect(); // Get the bounding rectangle of the collection list
+		const offsetX = x - rect.left; // Calculate the x-offset from the left of the collection list
+		const offsetY = y - rect.top; // Calculate the y-offset from the top of the collection list
+		collectionList.style.transformOrigin = `${offsetX}px ${offsetY}px`; // Set the transform origin of the collection list
+		collectionList.style.transform = 'scale(4)'; // Scale up the collection list
+	  } else {
+		collectionList.style.transform = 'scale(1)'; // Scale down the collection list to its original state
+	  }
+	});
+
+	
