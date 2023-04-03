3789081d7d701b7bc5d04c96fa5e29fe96d5c3e0
Queeny <shenk574@newschool.edu>
2023-04-03 17:00:51 -0400

    ?

--- projects/ElasticCollection/staticVer.js
+++ projects/ElasticCollection/staticVer.js
@@ -180,6 +180,20 @@ function topFunction() {
 }
 
 
+//mobile responsive layout
+let toggleMenu = document.querySelector(".mobile-menu");
+let headerStatus = document.querySelector(".header");
+
+// when user clicks on "menu", open and close the mobile navigation
+toggleMenu.addEventListener( "click", () => {
+    if (headerStatus.classList.contains("open")) {
+      headerStatus.classList.remove("open");
+    } else {
+      headerStatus.classList.add("open");
+    }
+  },
+  false
+);
 
 
 	
